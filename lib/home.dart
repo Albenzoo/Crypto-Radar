44f4b282fb45399a973af89533082821d763bf2d
Albenzoo <alberto.presenti@yahoo.it>
2021-10-24 18:15:04 +0200

    mock e grid

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:projectwallet/components/crypto_card.dart';
 import 'package:projectwallet/shared/constants.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,12 +14,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Future<http.Response> fetchCoin() async {
-    final response = await http.get(Uri.parse(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false'));
+    final response = await http.get(Uri.parse(baseUrl +
+        '/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=20&page=1&sparkline=false'));
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
       // then parse the JSON.
-      print(jsonDecode(response.body));
+      //print(jsonDecode(response.body));
       return response;
     } else {
       // If the server did not return a 200 OK response,
@@ -30,26 +31,44 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    //fetchCoin();
+    fetchCoin();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(
-          Icons.savings,
-        ),
-        title: Text('Crypto wallet'),
-        actions: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Icon(Icons.autorenew),
+        appBar: AppBar(
+          leading: Icon(
+            Icons.savings,
           ),
+          title: Text('Crypto wallet'),
+          actions: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Icon(Icons.autorenew),
+            ),
+          ],
+          backgroundColor: kPrimaryColor,
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20),
+                itemCount: 10,
+                itemBuilder: (BuildContext ctx, index) {
+                  return cryptoCard(context, "dd");
+                }))
+
+        /* ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          cryptoCard(context, "dd"),
         ],
-        backgroundColor: kPrimaryColor,
-      ),
-      body: Container(),
-    );
+      ), */
+        );
   }
 }
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 Color kPrimaryColor = Colors.purple;
 Color kSecondaryColor = Colors.grey.shade800;
+String baseUrl = "http://localhost:3001"; //https://api.coingecko.com
