c86401aeeddc0a7a0af1be06f5a10cf4d6bffcf9
Albenzoo <alberto.presenti@yahoo.it>
2021-10-31 18:44:11 +0100

    add futureBuilder to home

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:projectwallet/api/crypto_api.dart';
 import 'package:projectwallet/components/crypto_card.dart';
+import 'package:projectwallet/models/CryptoMarket.dart';
 import 'package:projectwallet/shared/constants.dart';
-import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -13,62 +12,58 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Future<http.Response> fetchCoin() async {
-    final response = await http.get(Uri.parse(baseUrl +
-        '/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=20&page=1&sparkline=false'));
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      //print(jsonDecode(response.body));
-      return response;
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load album');
-    }
-  }
-
   @override
   void initState() {
-    super.initState();
-    fetchCoin();
+    /* super.initState();
+    CryptoApi.fetchCoin(); */
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: Icon(
-            Icons.savings,
-          ),
-          title: Text('Crypto wallet'),
-          actions: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Icon(Icons.autorenew),
-            ),
-          ],
-          backgroundColor: kPrimaryColor,
+      appBar: AppBar(
+        leading: Icon(
+          Icons.savings,
         ),
-        body: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    childAspectRatio: 3 / 2,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20),
-                itemCount: 10,
-                itemBuilder: (BuildContext ctx, index) {
-                  return cryptoCard(context, "dd");
-                }))
-
-        /* ListView(
-        padding: EdgeInsets.all(16),
-        children: [
-          cryptoCard(context, "dd"),
+        title: Text('Crypto wallet'),
+        actions: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Icon(Icons.autorenew),
+          ),
         ],
-      ), */
-        );
+        backgroundColor: kPrimaryColor,
+      ),
+      body: FutureBuilder<List<CryptoMarket>>(
+        future: CryptoApi.fetchCoin(),
+        builder: (context, snapshot) {
+          final List<CryptoMarket>? cryptoList = snapshot.data;
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            default:
+              if (snapshot.hasError) {
+                return Center(child: Text('Some error occurred!'));
+              } else {
+                return buildCryptoGrid(cryptoList);
+              }
+          }
+        },
+      ),
+    );
   }
+
+  Widget buildCryptoGrid(List<CryptoMarket>? criptoList) => Padding(
+      padding: EdgeInsets.all(8.0),
+      child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          itemCount: 10,
+          itemBuilder: (BuildContext ctx, index) {
+            return cryptoCard(context, "dd");
+          }));
 }
