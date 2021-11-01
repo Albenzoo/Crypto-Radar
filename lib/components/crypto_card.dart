fa47c02dd2625df14d2d0c263809204087ab488b
Albenzoo <alberto.presenti@yahoo.it>
2021-11-01 20:29:17 +0100

    name parameter read

--- lib/components/crypto_card.dart
+++ lib/components/crypto_card.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:projectwallet/models/CryptoMarket.dart';
 import 'package:projectwallet/shared/constants.dart';
 import 'package:projectwallet/home.dart';
 
-Widget cryptoCard(BuildContext myContext, String text) {
+Widget cryptoCard(BuildContext myContext, CryptoMarket crypto) {
   return Card(
       child: Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      Text("Nome della crypto", style: TextStyle(fontSize: 24)),
+      Text(crypto.name, style: TextStyle(fontSize: 24)),
       const SizedBox(height: 12),
       Text("altre info",
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
