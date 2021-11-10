958930fb08942fb23c5e4d093fb709d02c01e683
Alberto <alberto.presenti@eng.it>
2021-11-10 16:51:49 +0100

    test with other pc

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # projectwallet
 
 Per avviare il progetto:
-flutter run -d chrome --web-port 8080
+flutter run -d chrome --web-port 8080 
+se va in errore provare ad impostare l'hostname (adattando il mock):
+flutter run -d chrome --web-port=8080 --web-hostname=127.0.0.1
+
 
 Per aggiornare la pagina cliccare "r" sul terminale
 
--- mocks/index.js
+++ mocks/index.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
