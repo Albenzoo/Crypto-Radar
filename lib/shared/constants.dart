fed60ef6f6555bc2fb770366a3bfdb441a098fff
Albenzoo <alberto.presenti@yahoo.it>
2021-11-13 20:25:20 +0100

    update theme colors

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 Color kPrimaryColor = Colors.deepPurpleAccent.shade400;
-Color kSecondaryColor = Colors.grey.shade800;
-String baseUrl = "http://localhost:3001"; //https://api.coingecko.com
+//Color kSecondaryColor = Colors.grey.shade800;
+Color kSecondaryColor = Colors.grey.shade200;
+//String baseUrl = "http://localhost:3001";
+String baseUrl = "https://api.coingecko.com";
