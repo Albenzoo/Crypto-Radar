abc171af5fc1d2d80a55cf3364d651309e2db697
Albenzoo <alberto.presenti@yahoo.it>
2021-11-14 00:18:45 +0100

    add pointer on login button

--- lib/action_button.dart
+++ lib/action_button.dart
@@ -3,40 +3,42 @@ import 'package:projectwallet/shared/constants.dart';
 import 'package:projectwallet/home.dart';
 
 Widget actionButton(BuildContext myContext, String text) {
-  return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          myContext,
-          MaterialPageRoute(builder: (context) => Home()),
-        );
-        print("Go to home page!");
-      },
-      child: Container(
-        height: 50,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: kPrimaryColor,
-          borderRadius: BorderRadius.all(
-            Radius.circular(25),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: kPrimaryColor.withOpacity(0.2),
-              spreadRadius: 4,
-              blurRadius: 7,
-              offset: Offset(0, 3),
+  return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              myContext,
+              MaterialPageRoute(builder: (context) => Home()),
+            );
+            print("Go to home page!");
+          },
+          child: Container(
+            height: 50,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.all(
+                Radius.circular(25),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: kPrimaryColor.withOpacity(0.2),
+                  spreadRadius: 4,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
             ),
-          ],
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+            child: Center(
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-          ),
-        ),
-      ));
+          )));
 }
