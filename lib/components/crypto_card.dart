9263931d3387990e27790f8e813ca6831b8b1a48
Albenzoo <alberto.presenti@yahoo.it>
2021-11-17 00:20:40 +0100

    card layout

--- lib/components/crypto_card.dart
+++ lib/components/crypto_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projectwallet/models/CryptoMarket.dart';
 import 'package:projectwallet/shared/constants.dart';
-import 'package:projectwallet/home.dart';
 
 Widget cryptoCard(BuildContext myContext, CryptoMarket crypto) {
   return Card(
@@ -23,37 +22,41 @@ Widget cryptoCard(BuildContext myContext, CryptoMarket crypto) {
           //color: kPrimaryColor,
         ),
         padding: EdgeInsets.all(12),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(4),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.shade600,
-                        offset: Offset(2, 2),
-                        blurRadius: 10,
-                        spreadRadius: 2,
-                      )
-                    ]),
-                height: 50,
-                width: 50,
-                child: Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Image.network(crypto.image),
+        child: Column(children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(4),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade600,
+                          offset: Offset(2, 2),
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                        )
+                      ]),
+                  height: 50,
+                  width: 50,
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Image.network(crypto.image),
+                  ),
                 ),
               ),
-            ),
-            Text(crypto.name + " (${crypto.symbol.toUpperCase()})",
-                style: TextStyle(fontSize: 18)),
-            const SizedBox(height: 8),
-            Text(crypto.currentPrice.toString() + "€",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          ],
-        )),
+              Text(crypto.name, style: TextStyle(fontSize: 18)),
+              Text(crypto.currentPrice.toString() + "€",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight:
+                          FontWeight.bold)), /* const SizedBox(height: 8), */
+            ],
+          )
+        ])),
   );
 }
