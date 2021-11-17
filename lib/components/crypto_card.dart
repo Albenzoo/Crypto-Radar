f182e196ee0a21bd26b97af70fac19fe1001451d
Albenzoo <alberto.presenti@yahoo.it>
2021-11-18 00:25:42 +0100

    fix card row column

--- lib/components/crypto_card.dart
+++ lib/components/crypto_card.dart
@@ -22,41 +22,64 @@ Widget cryptoCard(BuildContext myContext, CryptoMarket crypto) {
           //color: kPrimaryColor,
         ),
         padding: EdgeInsets.all(12),
-        child: Column(children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(4),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.shade600,
-                          offset: Offset(2, 2),
-                          blurRadius: 10,
-                          spreadRadius: 2,
-                        )
-                      ]),
-                  height: 50,
-                  width: 50,
-                  child: Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Image.network(crypto.image),
-                  ),
-                ),
+        child: Row(children: [
+          Padding(
+            padding: const EdgeInsets.all(4),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade600,
+                      offset: Offset(2, 2),
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                    )
+                  ]),
+              height: 50,
+              width: 50,
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Image.network(crypto.image),
               ),
-              Text(crypto.name, style: TextStyle(fontSize: 18)),
-              Text(crypto.currentPrice.toString() + "€",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight:
-                          FontWeight.bold)), /* const SizedBox(height: 8), */
-            ],
-          )
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 11),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(crypto.name, style: TextStyle(fontSize: 18)),
+                        Text(
+                            crypto.currentPrice
+                                    .toString()
+                                    .replaceAll(".", ",") +
+                                "€",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight
+                                    .bold)), /* const SizedBox(height: 8), */
+                      ],
+                    ),
+                    Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(crypto.symbol.toUpperCase(),
+                              style: TextStyle(
+                                  fontSize: 18, color: Colors.grey.shade800)),
+                        ])
+                  ]),
+            ),
+          ),
         ])),
   );
 }
