eb50782edb180fe6f5aa21004c19ed80ba7f0e28
Albenzoo <alberto.presenti@yahoo.it>
2021-12-05 16:45:43 +0100

    Add chart library and basic example

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,11 +1,15 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:projectwallet/components/line_chart.dart';
+import 'package:projectwallet/components/my_app_bar.dart';
+import 'package:projectwallet/shared/constants.dart';
 
 class Detail extends StatefulWidget {
   final String coinSymbol;
+  final String coinName;
 
-  Detail({required this.coinSymbol});
+  Detail({required this.coinSymbol, required this.coinName});
 
   @override
   _DetailState createState() => _DetailState();
@@ -21,9 +25,22 @@ class _DetailState extends State<Detail> {
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as Detail;
+    print("rebuild");
 
-    return Container(
-      child: Text(args.coinSymbol),
+    return Scaffold(
+      appBar: MyAppBar(
+        refreshPage: () => refresh(),
+      ),
+      body: Container(
+        //child: Text(args.coinSymbol),
+        child: LineCryptoChart(),
+      ),
     );
+  }
+
+  /* Refresh page */
+  void refresh() {
+    print("Aggiorno");
+    setState(() {});
   }
 }
