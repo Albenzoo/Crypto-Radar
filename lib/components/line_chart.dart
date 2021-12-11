ddda87ec877817205cfc76e4d252df4593c88d9a
Albenzoo <alberto.presenti@yahoo.it>
2021-12-11 17:53:37 +0100

    Desktop/Mobile layout Detail

--- lib/components/line_chart.dart
+++ lib/components/line_chart.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:projectwallet/api/crypto_api.dart';
 import 'package:projectwallet/models/MarketChartData.dart';
 import 'package:intl/intl.dart';
 
@@ -29,19 +28,22 @@ class _LineCryptoChartState extends State<LineCryptoChart> {
 
     return Stack(
       children: <Widget>[
-        AspectRatio(
-          aspectRatio: 1.70,
-          child: Container(
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(18),
+        Container(
+          //width: 300,
+          child: AspectRatio(
+            aspectRatio: 1.70,
+            child: Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(18),
+                  ),
+                  color: Color(0xff232d37)),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    right: 18.0, left: 12.0, top: 24, bottom: 12),
+                child: LineChart(
+                  showAvg ? avgData(prices) : mainData(prices),
                 ),
-                color: Color(0xff232d37)),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  right: 18.0, left: 12.0, top: 24, bottom: 12),
-              child: LineChart(
-                showAvg ? avgData(prices) : mainData(prices),
               ),
             ),
           ),
@@ -174,28 +176,6 @@ class _LineCryptoChartState extends State<LineCryptoChart> {
           },
           margin: 8,
         ),
-        /* leftTitles: SideTitles(
-          showTitles: true,
-          interval: 1,
-          getTextStyles: (context, value) => const TextStyle(
-            color: Color(0xff67727d),
-            fontWeight: FontWeight.bold,
-            fontSize: 15,
-          ),
-          getTitles: (value) {
-            switch (value.toInt()) {
-              case 1:
-                return '10k';
-              case 3:
-                return '30k';
-              case 5:
-                return '50k';
-            }
-            return '';
-          },
-          reservedSize: 32,
-          margin: 12,
-        ), */
       ),
       borderData: FlBorderData(
           show: true,
--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projectwallet/api/crypto_api.dart';
+import 'package:projectwallet/components/info_box.dart';
 import 'package:projectwallet/components/line_chart.dart';
 import 'package:projectwallet/components/my_app_bar.dart';
 import 'package:projectwallet/models/CoinInfo.dart';
@@ -28,14 +29,54 @@ class _DetailState extends State<Detail> {
 
   @override
   Widget build(BuildContext context) {
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
     return Scaffold(
-      appBar: MyAppBar(
-        refreshPage: () => refresh(),
-      ),
-      body: Container(
+        appBar: MyAppBar(
+          refreshPage: () => refresh(),
+        ),
+        body: queryData.size.width > 1100
+            ? desktopOrientation()
+            : mobileOrientation());
+  }
+
+  Widget desktopOrientation() {
+    return Row(
         //child: Text(args.coinSymbol),
-        child: chart(),
-      ),
+        children: [
+          Expanded(child: Align(alignment: Alignment.center, child: chart())),
+          Expanded(
+              child:
+                  Align(alignment: Alignment.center, child: informationBox())),
+        ]);
+  }
+
+  Widget mobileOrientation() {
+    return Column(
+        //child: Text(args.coinSymbol),
+        children: [
+          Expanded(child: Align(alignment: Alignment.center, child: chart())),
+          Expanded(
+              child:
+                  Align(alignment: Alignment.center, child: informationBox())),
+        ]);
+  }
+
+  Widget informationBox() {
+    return FutureBuilder<CoinInfo>(
+      future: coinInfo,
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Center(child: CircularProgressIndicator());
+          default:
+            if (snapshot.hasError) {
+              return Center(child: Text('Some error occurred!'));
+            } else {
+              return InfoBox(coinInfo: snapshot.data!);
+            }
+        }
+      },
     );
   }
 
